3f3c11ba3048840fc7e464d5b823d788e8f2d59c
znyang <xggcl@sina.com>
2015-12-02 22:29:09 +0800

    update test case

--- app/src/test/java/com/zen/android/rxjava/SimpleTest.java
+++ app/src/test/java/com/zen/android/rxjava/SimpleTest.java
@@ -2,6 +2,8 @@ package com.zen.android.rxjava;
 
 import org.junit.Test;
 
+import java.util.Iterator;
+
 import rx.Observable;
 import rx.schedulers.Schedulers;
 
@@ -13,41 +15,82 @@ import rx.schedulers.Schedulers;
  */
 public class SimpleTest {
 
-    private int buildValue() {
-        System.out.println("build");
-        return 1;
+    private void log(String log) {
+        System.out.println(Thread.currentThread().getName() + " - " + log);
+    }
+
+    private String buildValue() {
+        log("build");
+        return "1";
     }
 
     @Test
     public void testJust() throws Exception {
-        Observable obs = Observable.just(buildValue());
-        System.out.println("create");
-        obs.subscribe();
-        System.out.println("end");
+        Observable<String> obs = Observable.just(buildValue());
+        log("create");
+        obs.subscribe(this::log);
+        log("end");
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+        Observable<String> obs = Observable.create(subscriber -> {
+            subscriber.onNext(buildValue());
+            subscriber.onCompleted();
+        });
+        log("create");
+        obs.subscribe(this::log);
+        log("end");
     }
 
     @Test
     public void testDefer() throws Exception {
-        Observable<Integer> obs = Observable.defer(() -> {
-            System.out.println("start");
-            return Observable.just(buildValue());
-        });
-        System.out.println("create");
-        obs.subscribe(System.out::println);
-        System.out.println("end");
+        Observable<String> obs = Observable.defer(
+                () -> Observable.just(buildValue()));
+        log("create");
+        obs.subscribe(this::log);
+        log("end");
     }
 
     @Test
     public void testDeferAsync() throws Exception {
-        Observable<Integer> obs = Observable.defer(() -> {
-            System.out.println("start");
-            return Observable.just(buildValue());
-        });
-        System.out.println("create");
-        obs.observeOn(Schedulers.newThread())
-                .subscribe(System.out::println);
-        System.out.println("end");
+        Observable<String> obs = Observable.defer(
+                () -> Observable.just(buildValue()));
+        log("create");
+        obs.subscribeOn(Schedulers.newThread())
+                .subscribe(this::log);
+        log("end");
         Thread.sleep(10);
     }
 
+    @Test
+    public void testDeferSync() throws Exception {
+        Observable<String> obs = Observable.defer(
+                () -> Observable.just(buildValue(), buildValue()));
+        log("create");
+        Iterable<String> result = obs.subscribeOn(Schedulers.newThread())
+                .toBlocking().toIterable();
+        Observable.from(result)
+                .subscribe(this::log);
+        log("end");
+    }
+
+    @Test
+    public void testConcat() throws Exception {
+    Observable<String> disk = Observable.defer(() -> Observable.just(localData()));
+    Observable<String> network = Observable.defer(() -> Observable.just(remoteData()));
+
+    // 产生两次值事件，分别访问本地、网络
+    Observable.concat(disk, network).subscribe();
+    // 只产生一个值，如果本地没有就取网络
+    Observable.concat(disk, network).first().subscribe();
+    }
+
+    private String localData() {
+        return "1";
+    }
+
+    private String remoteData() {
+        return "2";
+    }
 }
